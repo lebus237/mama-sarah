a5a04a88b73fcbf83dfddb857ec7fda27cae9952
noudje ronny <noudjemambe@gmail.com>
2026-03-09 13:47:41 +0100

    changes

--- src/views/order/ui/OrderPage.tsx
+++ src/views/order/ui/OrderPage.tsx
@@ -97,10 +97,10 @@ export function OrderPage() {
                            {productItems.map((product, _index) => (
                               <Fade delay={200} triggerOnce key={product.id}>
                                  <div
-                                    className="cursor-pointer w-full h-40 xl:h-36 grid grid-cols-2 xl:grid-cols-7
-                                              max-md:border-b-2 border-gray-200  rounded-xl md:shadow-[0px_8px_24px_rgba(149,157,165,0.2)] hover:scale-105 transition-all duration-200"
+                                    className="items-stretch cursor-pointer w-full min-h-35 grid grid-cols-2 xl:grid-cols-7
+                                               max-md:border-b border-gray-200  md:rounded-xl md:shadow-[0px_8px_24px_rgba(149,157,165,0.2)] hover:scale-105 transition-all duration-200"
                                     onClick={() => openModal(product)}>
-                                    <aside className="flex flex-col  justify-between xl:col-span-5 p-3">
+                                    <aside className="flex flex-col  justify-between xl:col-span-5 md:p-3 h-full ">
                                        <section className="space-y-2">
                                           <h5 className="xl:text-xl font-bold line-clamp-2 capitalize font-cabin">
                                              {product.designation}
@@ -109,20 +109,21 @@ export function OrderPage() {
                                              {_.truncate(product.description, { length: 70 })}
                                           </p>
                                        </section>
-                                       <section className="flex md:justify-end ">
+                                       <section className="max-md:flex max-md:flex-1  md:justify-end mt-1 ">
                                           <PriceDisplay
                                              amount={parseFloat(product.price.toString())}
-                                             className="text-tertiary leading-[100%] lg:text-lg font-bebas tracking-wide"
+                                             className="
+                                             text-tertiary leading-[100%] lg:text-lg font-bebas tracking-wide"
                                           />
                                        </section>
                                     </aside>
-                                    <aside className="xl:col-span-2 flex items-center border-l border-gray-50 border-dashed">
-                                       <figure className="relative w-full h-28 md:h-full max-md:border-2 max-md:border-gray-200 rounded-md">
+                                    <aside className="h-full xl:col-span-2 flex justify-end items-start border-l border-gray-50 border-dashed rounded-lg">
+                                       <figure className="relative w-[60%] h-[90%] md:w-full md:h-full max-md:border-2 max-md:border-gray-200 rounded-xl overflow-hidden">
                                           <Image
                                              src={product.imageUrl}
                                              alt="thumbnail"
                                              fill
-                                             className="object-cover rounded-tr-xl rounded-br-xl "
+                                             className="object-cover "
                                           />
                                           <div
                                              className="rounded-full text-secondary bg-white w-10 h-10 absolute bottom-1.5 right-1.5 flex items-center justify-center shadow-[0px_7px_29px_0px_rgba(100,100,111,0.2)]"
--- src/widgets/layout/ui/components/Header.tsx
+++ src/widgets/layout/ui/components/Header.tsx
@@ -3,15 +3,15 @@ import Link from 'next/link'
 
 export function Header() {
    return (
-      <div className="flex justify-between items-center">
+      <div className="flex justify-between items-cente ">
          <aside>
             <h1 className="text-3xl font-bold">Logo Food</h1>
          </aside>
-         <nav className="xl:space-x-6 uppercase">
+         <nav className=" hidden md:block xl:space-x-6 uppercase">
             <Link href="/">Acceuil</Link>
             <Link href="/order">Commander</Link>
          </nav>
-         <aside>
+         <aside className=''>
             <OrderCounterDisplay />
          </aside>
       </div>
