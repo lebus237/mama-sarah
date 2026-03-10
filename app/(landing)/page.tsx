4eda9ef9f04c243e20c50e5e54691678724d0779
melvis8 <melvisnajela@gmail.com>
2026-03-10 17:46:58 +0100

    (fix):creating tte hero section of the welcome page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,8 +1,14 @@
 import { getProducts } from '@/entities/product'
 import { HomeProductList } from '@/views/home'
+import HeroSection from '@/views/home/sections/HeroSection'
 
 export default function Page() {
    const products = getProducts()
 
-   return <HomeProductList products={products} />
+   return (
+      <div>
+         <HeroSection/>
+         <HomeProductList products={products} />
+      </div>
+   )
 }
